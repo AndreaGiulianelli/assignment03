7425e82e1e13a8bca8b0eb33181a2701bfd84f6e
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-11 00:35:04 +0200

    refactor: refactor view actor

--- ex1/assignment/physicssimulator/src/main/scala/view/ViewActor.scala
+++ ex1/assignment/physicssimulator/src/main/scala/view/ViewActor.scala
@@ -9,32 +9,26 @@ import controller.Coordinator
 object ViewActor:
   enum Command:
     case Start(w: Int, h: Int, coordinator: ActorRef[Coordinator.Command])
-    case Update(positions: Seq[P2d], vt: Double, iter: Long, boundary: Boundary)
+    case Update(positions: Seq[P2d], vt: Double, iter: Int, boundary: Boundary)
     case Terminated
 
   def apply(): Behavior[Command] = init()
 
-  // todo: check, i think that the stashing here is not necessary considering that the view actor is the first created and the only one that the guardian send a message. It starts all from here
   private def init(): Behavior[Command] =
-    Behaviors.withStash[Command](100) { stash =>
-      Behaviors.receive { (ctx, msg) =>
-        msg match
-          case Command.Start(w, h, coordinator) =>
-            val viewer = SimulationViewer(w, h, coordinator)
-            viewer.display()
-            stash.unstashAll(initialized(viewer))
-          case other =>
-            stash.stash(other)
-            Behaviors.same
-      }
+    Behaviors.receivePartial { case (ctx, Command.Start(w, h, coordinator)) =>
+      val viewer = SimulationViewer(w, h, coordinator)
+      viewer.display()
+      initialized(viewer)
     }
 
   private def initialized(viewer: SimulationViewer): Behavior[Command] = Behaviors.receivePartial { (ctx, msg) =>
     msg match
       case Command.Update(positions, vt, iter, boundary) =>
+        ctx.log.info("VIEW ACTOR: received update")
         viewer.update(positions, vt, iter, boundary)
         Behaviors.same
       case Command.Terminated =>
+        ctx.log.info("VIEW ACTOR: received TERMINATION")
         viewer.simulationEnd()
         Behaviors.stopped
   }
