2e8e032763a0f1d8ce40a17c83af5f5bdc3f4a2b
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-11 00:35:46 +0200

    chore: add launcher

---
+++ ex1/assignment/physicssimulator/src/main/scala/Launcher.scala
@@ -0,0 +1,20 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import controller.Coordinator
+import model.Boundary
+import view.ViewActor
+
+object Launcher:
+  @main def main(): Unit =
+    ActorSystem(
+      Behaviors.setup { ctx =>
+        val iterations = 50000
+        val bodyNumber = 500
+        val boundary = Boundary(-6, -6, 6, 6)
+        val viewActor = ctx.spawn(ViewActor(), "view")
+        val coordinator = ctx.spawn(Coordinator(viewActor, iterations, bodyNumber, boundary), "coordinator")
+        viewActor ! ViewActor.Command.Start(620, 620, coordinator)
+        Behaviors.empty
+      },
+      "physics-simulator"
+    )
