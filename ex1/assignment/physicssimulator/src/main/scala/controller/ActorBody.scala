15f2d1a3f96015c2e1edc0059ad19ebd7e3a2031
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-11 00:35:30 +0200

    chore: SOLVED DEADLOCK, BUT VERY UGLY, NEED REFACTOR

--- ex1/assignment/physicssimulator/src/main/scala/controller/ActorBody.scala
+++ ex1/assignment/physicssimulator/src/main/scala/controller/ActorBody.scala
@@ -4,16 +4,18 @@ import model.{Body, Boundary, P2d, V2d}
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import controller.ActorBody.Command
+import controller.utils.Util
 
 object ActorBody:
   trait Command
   case class Start(actorBodies: Set[ActorRef[Command]]) extends Command
-  case class PosUpdated() extends Command
-  private case class State(pos: P2d, mass: Double) extends Command
-  private case class ForceUpdated() extends Command
+  case class PosUpdated(iteration: Int) extends Command
+  private case class State(iteration: Int, pos: P2d, mass: Double) extends Command
+  private case class ForceUpdated(iteration: Int) extends Command
 
   enum Message:
-    case UpdatedPos(pos: P2d)
+    case UpdatedPos(iteration: Int, pos: P2d)
 
   def apply(
       body: Body,
@@ -36,74 +38,149 @@ case class ActorBody(
   import ActorBody.*
   import monocle.syntax.all._
 
+  private var iteration = 1
+  private var currentResponses = 0
+  private var repulsiveForce = V2d()
+
   val created: Behavior[Command] =
-    Behaviors.withStash(100) { stash =>
+    Behaviors.withStash(10000) { stash =>
       Behaviors.receive { (ctx, msg) =>
         msg match
           case Start(bodyRefs) =>
-            sendToAll(State(body.pos, body.mass))
-            stash.unstashAll(this.focus(_.actorBodies).replace(bodyRefs).force())
+            ctx.log.info(s"BODY ACTOR - ${body.id}: received start")
+            val refs = bodyRefs - ctx.self
+            Util.sendToAll(refs, State(1, body.pos, body.mass))
+            stash.unstashAll(this.focus(_.actorBodies).replace(refs).force())
           case other =>
             stash.stash(other)
             Behaviors.same
       }
     }
 
-  private def force(repulsiveForce: V2d = V2d(), currentResponses: Int = 0): Behavior[Command] =
-    Behaviors.withStash(100) { stash =>
+//  private def force(repulsiveForce: V2d = V2d(), currentResponses: Int = 0, iteration: Int = 1): Behavior[Command] =
+//    Behaviors.withStash(actorBodies.size) { stash =>
+//      Behaviors.receive { (ctx, msg) =>
+//        msg match
+//          case State(`iteration`, pos, mass) =>
+//            //ctx.log.info(s"BODY ACTOR - ${body.id}: received STATE")
+//            val currentResponsesUpdated = currentResponses + 1
+//            val repulsiveForceUpdated = repulsiveForce + body.repulsiveForceBy(pos, mass).getOrElse(V2d())
+//            if currentResponsesUpdated == actorBodies.size then
+//              body = body.accelerate(repulsiveForceUpdated + body.currentFrictionForce)
+//              Util.sendToAll(actorBodies, ForceUpdated(iteration))
+//              ctx.log.info(s"BODY ACTOR - ${body.id}: it: $iteration force updated")
+//              stash.unstashAll(waitForces(iteration = iteration))
+//            else force(repulsiveForceUpdated, currentResponsesUpdated, iteration)
+//          case other =>
+//            stash.stash(other)
+//            Behaviors.same
+//      }
+//    }
+
+  private def force(): Behavior[Command] =
+    Behaviors.withStash(actorBodies.size) { stash =>
       Behaviors.receive { (ctx, msg) =>
         msg match
-          case State(pos, mass) =>
-            val currentResponsesUpdated = currentResponses + 1
-            val repulsiveForceUpdated = repulsiveForce + body.repulsiveForceBy(pos, mass).get
-            if currentResponsesUpdated == actorBodies.size then
-              body = body.accelerate(repulsiveForceUpdated + body.currentFrictionForce)
-              sendToAll(ForceUpdated())
+          case State(it, pos, mass) if it == iteration =>
+            //ctx.log.info(s"BODY ACTOR - ${body.id}: received STATE")
+            currentResponses = currentResponses + 1
+            repulsiveForce = repulsiveForce + body.repulsiveForceBy(pos, mass).getOrElse(V2d())
+            if currentResponses == actorBodies.size then
+              body = body.accelerate(repulsiveForce + body.currentFrictionForce)
+              Util.sendToAll(actorBodies, ForceUpdated(iteration))
+              ctx.log.info(s"BODY ACTOR - ${body.id}: it: $iteration force updated")
+              currentResponses = 0
+              repulsiveForce = V2d()
               stash.unstashAll(waitForces())
-            else force(repulsiveForceUpdated, currentResponsesUpdated)
+            else Behaviors.same
           case other =>
             stash.stash(other)
             Behaviors.same
       }
     }
 
-  private def waitForces(currentResponses: Int = 0): Behavior[Command] =
-    Behaviors.withStash(100) { stash =>
+//  private def waitForces(iteration: Int, currentResponses: Int = 0): Behavior[Command] =
+//    Behaviors.withStash(actorBodies.size) { stash =>
+//      Behaviors.receive { (ctx, msg) =>
+//        msg match
+//          case ForceUpdated(`iteration`) =>
+//            //ctx.log.info(s"BODY ACTOR - ${body.id}: force barrier")
+//            val currentResponsesUpdated = currentResponses + 1
+//            if currentResponsesUpdated == actorBodies.size then
+//              body = body
+//                .updateVelocity(dt)
+//                .updatePos(dt)
+//                .checkAndSolveBoundaryCollision(boundary)
+//              simulation ! Message.UpdatedPos(iteration, body.pos)
+//              Util.sendToAll(actorBodies, PosUpdated(iteration))
+//              ctx.log.info(s"BODY ACTOR - ${body.id}: it: $iteration position updated")
+//              stash.unstashAll(waitPos(iteration = iteration))
+//            else waitForces(iteration, currentResponsesUpdated)
+//          case other =>
+//            stash.stash(other)
+//            Behaviors.same
+//      }
+//    }
+
+  private def waitForces(): Behavior[Command] =
+    Behaviors.withStash(actorBodies.size) { stash =>
       Behaviors.receive { (ctx, msg) =>
         msg match
-          case ForceUpdated() =>
-            val currentResponsesUpdated = currentResponses + 1
-            if currentResponsesUpdated == actorBodies.size then
+          case ForceUpdated(it) if it == iteration =>
+            //ctx.log.info(s"BODY ACTOR - ${body.id}: force barrier")
+            currentResponses = currentResponses + 1
+            if currentResponses == actorBodies.size then
               body = body
                 .updateVelocity(dt)
                 .updatePos(dt)
                 .checkAndSolveBoundaryCollision(boundary)
-              simulation ! Message.UpdatedPos(body.pos)
-              sendToAll(PosUpdated())
+              simulation ! Message.UpdatedPos(iteration, body.pos)
+              Util.sendToAll(actorBodies, PosUpdated(iteration))
+              ctx.log.info(s"BODY ACTOR - ${body.id}: it: $iteration position updated")
+              currentResponses = 0
               stash.unstashAll(waitPos())
-            else waitForces(currentResponsesUpdated)
+            else Behaviors.same
           case other =>
             stash.stash(other)
             Behaviors.same
       }
     }
 
-  private def waitPos(currentResponses: Int = 0): Behavior[Command] =
-    Behaviors.withStash(100) { stash =>
+//  private def waitPos(iteration: Int, currentResponses: Int = 0): Behavior[Command] =
+//    Behaviors.withStash(actorBodies.size) { stash =>
+//      Behaviors.receive { (ctx, msg) =>
+//        msg match
+//          case PosUpdated(`iteration`) =>
+//            //ctx.log.info(s"BODY ACTOR - ${body.id}: position barrier")
+//            val currentResponsesUpdated = currentResponses + 1
+//            if currentResponsesUpdated == actorBodies.size + 1 then // + 1 consider the simulation ack
+//              val newIteration = iteration + 1
+//              Util.sendToAll(actorBodies, State(newIteration, body.pos, body.mass))
+//              ctx.log.info(s"BODY ACTOR - ${body.id}, it: $iteration: new iter")
+//              stash.unstashAll(force(iteration = newIteration))
+//            else waitPos(iteration, currentResponsesUpdated)
+//          case other =>
+//            stash.stash(other)
+//            Behaviors.same
+//      }
+//    }
+
+  private def waitPos(): Behavior[Command] =
+    Behaviors.withStash(actorBodies.size) { stash =>
       Behaviors.receive { (ctx, msg) =>
         msg match
-          case PosUpdated() =>
-            val currentResponsesUpdated = currentResponses + 1
-            if currentResponsesUpdated == actorBodies.size + 1 then // + 1 consider the simulation ack
-              sendToAll(State(body.pos, body.mass))
+          case PosUpdated(it) if it == iteration =>
+            //ctx.log.info(s"BODY ACTOR - ${body.id}: position barrier")
+            currentResponses = currentResponses + 1
+            if currentResponses == actorBodies.size + 1 then // + 1 consider the simulation ack
+              ctx.log.info(s"BODY ACTOR - ${body.id}, it: $iteration: new iter")
+              iteration = iteration + 1
+              Util.sendToAll(actorBodies, State(iteration, body.pos, body.mass))
+              currentResponses = 0
               stash.unstashAll(force())
-            else waitPos(currentResponsesUpdated)
+            else Behaviors.same
           case other =>
             stash.stash(other)
             Behaviors.same
-
       }
     }
-
-  private def sendToAll(msg: Command): Unit =
-    for actorRef <- actorBodies do actorRef ! msg
--- ex1/assignment/physicssimulator/src/main/scala/controller/Simulation.scala
+++ ex1/assignment/physicssimulator/src/main/scala/controller/Simulation.scala
@@ -19,7 +19,7 @@ object Simulation:
     case UpdatedBodyPosition(updatedPos: ActorBody.Message)
 
   enum Message:
-    case Update(iteration: Int, positions: Seq[P2d])
+    case Update(iteration: Int, virtualTime: Double, positions: Seq[P2d])
     case Terminated
 
   def apply(coordinator: ActorRef[Message]): Behavior[Command] = Behaviors.setup { ctx =>
@@ -30,55 +30,60 @@ case class Simulation(
     coordinator: ActorRef[Simulation.Message],
     maxIterations: Int = 10,
     bodyRefs: Set[ActorRef[ActorBody.Command]] = Set(),
-    iteration: Int = 1
+    iteration: Int = 1,
+    virtualTime: Double = 0
 ):
   import Simulation.Command
   import Simulation.Message
   import monocle.syntax.all._
 
-  val created: Behavior[Command] = Behaviors.receivePartial { (ctx, msg) =>
-    msg match
-      case Command.Start(iterations, nBodies, boundary) =>
-        val adapter = ctx.messageAdapter(Command.UpdatedBodyPosition.apply)
-        val actorRefs =
-          for
-            i <- 0 until nBodies
-            x = boundary.x0 * 0.25 + Random.nextDouble() * (boundary.x1 - boundary.x0) * 0.25
-            y = boundary.y0 * 0.25 + Random.nextDouble() * (boundary.y1 - boundary.y0) * 0.25
-            body = Body(id = i, pos = P2d(x, y), mass = DEFAULT_MASS)
-            bodyRef = ctx.spawn(ActorBody(body, adapter, DELTA_TIME, boundary), s"body-$i")
-          yield bodyRef
-        for actorRef <- actorRefs do actorRef ! ActorBody.Start(actorRefs.toSet)
-        this
-          .focus(_.maxIterations)
-          .replace(iterations)
-          .focus(_.bodyRefs)
-          .replace(actorRefs.toSet)
-          .updating()
+  val created: Behavior[Command] = Behaviors.receivePartial {
+    case (ctx, Command.Start(iterations, nBodies, boundary)) =>
+      ctx.log.info("SIMULATION ACTOR: received start")
+      val adapter = ctx.messageAdapter(Command.UpdatedBodyPosition.apply)
+      val actorRefs =
+        for
+          i <- 0 until nBodies
+          x = boundary.x0 * 0.25 + Random.nextDouble() * (boundary.x1 - boundary.x0) * 0.25
+          y = boundary.y0 * 0.25 + Random.nextDouble() * (boundary.y1 - boundary.y0) * 0.25
+          body = Body(id = i, pos = P2d(x, y), mass = DEFAULT_MASS)
+          bodyRef = ctx.spawn(ActorBody(body, adapter, DELTA_TIME, boundary), s"body-$i")
+        yield bodyRef
+      for actorRef <- actorRefs do actorRef ! ActorBody.Start(actorRefs.toSet)
+      this
+        .focus(_.maxIterations)
+        .replace(iterations)
+        .focus(_.bodyRefs)
+        .replace(actorRefs.toSet)
+        .updating()
   }
 
   private def updating(positions: Seq[P2d] = Seq()): Behavior[Command] =
-    Behaviors.receivePartial { (ctx, msg) =>
-      msg match
-        case Command.UpdatedBodyPosition(ActorBody.Message.UpdatedPos(pos)) =>
-          val positionsUpdated = pos +: positions
-          positionsAndIterationCheck(positionsUpdated)
-        case Command.Stop => stop(positions)
+    Behaviors.receivePartial {
+      case (ctx, Command.UpdatedBodyPosition(ActorBody.Message.UpdatedPos(iteration, pos))) =>
+        ctx.log.info("SIMULATION ACTOR: received update from body - " + iteration)
+        val positionsUpdated = pos +: positions
+        positionsAndIterationCheck(positionsUpdated)
+      case (ctx, Command.Stop) => stop(positions)
     }
 
   private def positionsAndIterationCheck(positions: Seq[P2d]): Behavior[Command] =
     if positions.size == bodyRefs.size then
-      coordinator ! Message.Update(iteration, positions)
+      coordinator ! Message.Update(iteration, virtualTime, positions)
       if iteration == maxIterations then coordinator ! Message.Terminated
-      else for bodyRef <- bodyRefs do bodyRef ! ActorBody.PosUpdated()
-      this.focus(_.iteration).modify(_ + 1).updating()
+      else for bodyRef <- bodyRefs do bodyRef ! ActorBody.PosUpdated(iteration)
+      this
+        .focus(_.iteration)
+        .modify(_ + 1)
+        .focus(_.virtualTime)
+        .modify(_ + DELTA_TIME)
+        .updating()
     else updating(positions)
 
-  private def stop(snapshot: Seq[P2d]): Behavior[Command] = Behaviors.receivePartial { (ctx, msg) =>
-    msg match
-      case Command.UpdatedBodyPosition(ActorBody.Message.UpdatedPos(pos)) =>
-        val snapshotUpdated = pos +: snapshot
-        stop(snapshotUpdated)
-      case Command.Resume =>
-        positionsAndIterationCheck(snapshot)
+  private def stop(snapshot: Seq[P2d]): Behavior[Command] = Behaviors.receivePartial {
+    case (ctx, Command.UpdatedBodyPosition(ActorBody.Message.UpdatedPos(iteration, pos))) =>
+      val snapshotUpdated = pos +: snapshot
+      stop(snapshotUpdated)
+    case (ctx, Command.Resume) =>
+      positionsAndIterationCheck(snapshot)
   }
