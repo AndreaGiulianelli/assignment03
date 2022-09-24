4187649abe41a04de25b4f48c286177dbd2eadf6
Andrea Giulianelli <andrygiulio@gmail.com>
2022-09-24 19:01:05 +0200

    refactor: refactor ViewActor

--- ex1/assignment/physicssimulator/src/main/scala/view/ViewActor.scala
+++ ex1/assignment/physicssimulator/src/main/scala/view/ViewActor.scala
@@ -6,6 +6,7 @@ import model.{Boundary, P2d}
 import view.gui.SimulationViewer
 import controller.Coordinator
 
+/** Module that model the ViewActor actor. */
 object ViewActor:
   enum Command:
     case Start(w: Int, h: Int, coordinator: ActorRef[Coordinator.Command])
@@ -15,20 +16,18 @@ object ViewActor:
   def apply(): Behavior[Command] = init()
 
   private def init(): Behavior[Command] =
-    Behaviors.receivePartial { case (ctx, Command.Start(w, h, coordinator)) =>
+    Behaviors.receivePartial { case (_, Command.Start(w, h, coordinator)) =>
       val viewer = SimulationViewer(w, h, coordinator)
       viewer.display()
       initialized(viewer)
     }
 
-  private def initialized(viewer: SimulationViewer): Behavior[Command] = Behaviors.receivePartial { (ctx, msg) =>
+  private def initialized(viewer: SimulationViewer): Behavior[Command] = Behaviors.receivePartial { (_, msg) =>
     msg match
       case Command.Update(positions, vt, iter, boundary) =>
-        //ctx.log.info("VIEW ACTOR: received update")
         viewer.update(positions, vt, iter, boundary)
         Behaviors.same
       case Command.Terminated =>
-        //ctx.log.info("VIEW ACTOR: received TERMINATION")
         viewer.simulationEnd()
         Behaviors.stopped
   }
