4b9dcb442be8bc0305965055ab1b649abeb1b5e8
Andrea Giulianelli <andrygiulio@gmail.com>
2022-10-11 11:47:03 +0200

    chore: add deploy functionalities

--- ex2/assignment/flood-control-smartcity/src/main/scala/util/Utils.scala
+++ ex2/assignment/flood-control-smartcity/src/main/scala/util/Utils.scala
@@ -2,9 +2,22 @@ package util
 
 import akka.actor.typed.{ActorSystem, Behavior}
 import com.typesafe.config.ConfigFactory
+import firestation.Firestation
+import model.CityModel.{FirestationService, PluviometerSensor, Zone}
+import pluviometer.Pluviometer
+import zone.ZoneControl
 
 object Utils:
-  def startNode[X](configFile: String = "cluster-config")(role: String, port: Int)(
+  def deployZone(zone: Zone): Unit =
+    startNode("zone", 2551 + zone.zoneId)(ZoneControl())
+
+  def deployFireStation(firestation: FirestationService): Unit =
+    startNode("firestation", 7000 + firestation.associatedZone.zoneId)(Firestation())
+
+  def deployPluviometer(pluviometer: PluviometerSensor): Unit =
+    startNode("sensor", 9000 + pluviometer.pluviometerId)(Pluviometer())
+
+  private def startNode[X](role: String, port: Int, configFile: String = "cluster-config")(
       root: => Behavior[X]
   ): ActorSystem[X] =
     val config = ConfigFactory
