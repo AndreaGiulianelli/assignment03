d105f44bc52cef694bd6b8c34273bb78faa25e0b
Andrea Giulianelli <andrygiulio@gmail.com>
2022-10-15 18:17:06 +0200

    chore: added some todos

--- ex2/assignment/flood-control-smartcity/src/main/scala/firestation/Firestation.scala
+++ ex2/assignment/flood-control-smartcity/src/main/scala/firestation/Firestation.scala
@@ -3,8 +3,12 @@ package firestation
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.*
 
+/*
+  todo: gui is launched after the firestation received ok from the zone. Inoltre fare in modo che subito dopo la zona invii anche il suo stato così al volo in modo tale che si setti.
+ */
+
 object Firestation:
   enum Command:
-    case TODO
+    case TODO //todo
 
   def apply(): Behavior[Command] = Behaviors.empty
