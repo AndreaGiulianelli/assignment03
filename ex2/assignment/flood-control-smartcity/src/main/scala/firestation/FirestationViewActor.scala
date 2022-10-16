fd29f2266139690e8a1431fd91e87b9315e1674b
Andrea Giulianelli <andrygiulio@gmail.com>
2022-10-16 16:36:08 +0200

    fix: fix update command

--- ex2/assignment/flood-control-smartcity/src/main/scala/firestation/FirestationViewActor.scala
+++ ex2/assignment/flood-control-smartcity/src/main/scala/firestation/FirestationViewActor.scala
@@ -4,12 +4,12 @@ import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import firestation.gui.FirestationViewer
 import firestation.Firestation
-import model.CityModel.Zone
+import model.CityModel.{FirestationService, Zone}
 
 object FirestationViewActor:
   enum Command:
     case Start(zone: Zone, width: Int, height: Int, firestation: ActorRef[Firestation.Command])
-    case Update(zone: Zone)
+    case UpdateFirestation(firestation: FirestationService)
 
   def apply(): Behavior[Command] = init()
 
@@ -21,7 +21,7 @@ object FirestationViewActor:
     }
 
   private def initialized(viewer: FirestationViewer): Behavior[Command] = Behaviors.receiveMessagePartial {
-    case Command.Update(zone) =>
-      viewer.update(zone)
+    case Command.UpdateFirestation(firestation) =>
+      viewer.update(firestation)
       Behaviors.same
   }
