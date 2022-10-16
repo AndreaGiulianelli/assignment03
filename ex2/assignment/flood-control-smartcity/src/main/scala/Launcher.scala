fbf462005a3131c8c04e3149f00fc9fe214077f3
Andrea Giulianelli <andrygiulio@gmail.com>
2022-10-16 22:58:37 +0200

    chore: tuned launcher

--- ex2/assignment/flood-control-smartcity/src/main/scala/Launcher.scala
+++ ex2/assignment/flood-control-smartcity/src/main/scala/Launcher.scala
@@ -9,8 +9,8 @@ object Launcher:
 
   @main def main(): Unit =
     given SmartCityParameters with
-      override val zones: Int = 2
-      override val sensorPerZone: Int = 1
+      override val zones: Int = 5
+      override val sensorPerZone: Int = 4
       override val senseRate: Int = 1000
     val city = generateCity()
     city.zones.foreach(deployZone)
